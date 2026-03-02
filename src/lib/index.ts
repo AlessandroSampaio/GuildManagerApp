8fe5a3808b652b81d9d86d31e0ced3e1df4f0ba2
AlessandroSampaio <alessandrosampaio521@gmail.com>
2026-03-02 15:38:05 -0300

    fix: center export queries from lib/index

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,10 @@
 export { queryClient } from "./query-client";
 export { reportKeys, wclKeys } from "./query-keys";
+export {
+  // queryOptions factories (for prefetch / server components)
+  wclStatusOptions,
+  // Query hooks
+  useWclStatus,
+  // Mutation hooks
+  useRevokeWcl,
+} from "./queries/wcl-auth";
